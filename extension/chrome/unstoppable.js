90ead26e936b6eb4d248546bdc69ddadec79eb98
Demian <dmyanster@gmail.com>
2019-03-09 00:56:40 -0300

    initial commit

---
+++ extension/chrome/unstoppable.js
@@ -0,0 +1,20 @@
+// console.log("comecando");
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    if (mutation.attributeName === "class") {
+		$cmp = $(mutation.target)
+		// console.log("tagName: ", $cmp.prop("tagName"), " | overflow: ", $cmp.css('overflow'));
+		if($cmp.css('overflow') == 'hidden') {
+		   	$cmp.attr("style", "overflow: auto!important");
+		   	console.log("overflow DISABLED");
+		} else {
+			// console.log("overflow not found");
+		}
+    }
+  });
+});
+jQuery.each( [$('html'), $('body')], function( i, cmp ) {
+	observer.observe(cmp[0], {
+	  attributes: true
+	});
+});
